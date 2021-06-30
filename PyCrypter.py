238b6d724d65affbc7450d453cbe9b7f7ade5ebd
doop <75194878+7uk@users.noreply.github.com>
2021-07-01 00:19:59 +0100

    PyCrypt

--- PyCrypter.py
+++ PyCrypter.py
@@ -4,26 +4,28 @@ from sys import argv
 import colorama
 from colorama import Fore
 
-OFFSET = 10
-VARIABLE_NAME = '__PYCRYPTER_HA' * 100
+crypter_offset = 10
+crypter_mark = '__PYCRYPTER_HA' * 100
 
-def obfuscate(content):
+def pycrypt(content):
     b64_content = base64.b64encode(content.encode()).decode()
     index = 0
-    code = f'{VARIABLE_NAME} = ""\n'
-    for _ in range(int(len(b64_content) / OFFSET) + 1):
+    code = f'{crypter_mark} = ""\n'
+
+    for _ in range(int(len(b64_content) / crypter_offset) + 1):
         _str = ''
-        for char in b64_content[index:index + OFFSET]:
+        for char in b64_content[index:index + crypter_offset]:
             byte = str(hex(ord(char)))[2:]
             if len(byte) < 2:
                 byte = '0' + byte
             _str += '\\x' + str(byte)
-        code += f'{VARIABLE_NAME} += "{_str}"\n'
-        index += OFFSET
-    code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode({VARIABLE_NAME}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
+        code += f'{crypter_mark} += "{_str}"\n'
+        index += crypter_offset
+    code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode({crypter_mark}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
+
     return code
 
-def main():
+def pycrypt_startup():
     print(f"""
                                    {Fore.BLUE}w              
 {Fore.RED}88b.{Fore.RESET} {Fore.MAGENTA}Yb  dP{Fore.RESET} {Fore.BLUE}.d8b{Fore.RESET} {Fore.CYAN}8d8b{Fore.RESET} {Fore.YELLOW}Yb  dP{Fore.RESET} {Fore.GREEN}88b.{Fore.RESET} {Fore.BLUE}w8ww{Fore.RESET} {Fore.CYAN}.d88b{Fore.RESET} {Fore.YELLOW}8d8b{Fore.RESET} 
@@ -33,27 +35,25 @@ def main():
 	
 	""")
     print()
+	
     try:
         path = argv[1]
+		
         if not os.path.exists(path):
             print(f'[{Fore.RED}x{Fore.RESET}] Selected File {Fore.RED}NOT FOUND!{Fore.RESET}')
             exit()
-
         if not os.path.isfile(path) or not path.endswith('.py'):
             print(f'[{Fore.RED}x{Fore.RESET}] File Type {Fore.RED}INVALID!{Fore.RESET}')
             exit()
-        
+			
         with open(path, 'r', encoding='utf-8', errors='ignore') as file:
             file_content = file.read()
-
-        obfuscated_content = obfuscate(file_content)
-
+        pycrypt_content = pycrypt(file_content)
         with open(f'{path.split(".")[0]}_pycrypted.py', 'w') as file:
-            file.write(obfuscated_content)
-
+            file.write(pycrypt_content)
         print(f'[{Fore.GREEN}+{Fore.RESET}] File Has been PyCrypted {Fore.GREEN}SUCCESSFULLY!{Fore.RESET}')
     except:
         print(f'[{Fore.RED}x{Fore.RESET}] PyCrypter Correct Usage: {Fore.YELLOW}{argv[0]} <file>{Fore.RESET}')
 
 if __name__ == '__main__':
-    main()
+    pycrypt_startup()

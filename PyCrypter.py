8d708bd14d4505fafbb4eabd1a421c9bd36501e5
doop <75194878+codeuk@users.noreply.github.com>
2021-11-30 18:25:01 +0000

    optimisation

--- PyCrypter.py
+++ PyCrypter.py
@@ -1,14 +1,14 @@
 import os
+import time
 import base64
-from sys import argv
 import colorama
+from sys import argv
 from colorama import Fore
 
-crypter_offset = 10
-crypter_mark = '__PYCRYPTER_HA' * 100
+crypter_offset = 8
+crypter_mark = '__cryptshitter_' * 100
 
 def pycrypt(content):
-	
     b64_content = base64.b64encode(content.encode()).decode()
     index = 0
     code = f'{crypter_mark} = ""\n'
@@ -18,17 +18,22 @@ def pycrypt(content):
 	
         for char in b64_content[index:index + crypter_offset]:
             byte = str(hex(ord(char)))[2:]
-	
             if len(byte) < 2:
                 byte = '0' + byte
-		
             _str += '\\x' + str(byte)
-	
         code += f'{crypter_mark} += "{_str}"\n'
         index += crypter_offset
-	
     code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode({crypter_mark}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
-
+    for _ in range(int(len(b64_content) / crypter_offset) + 1):
+        _str = ''
+	
+        for char in b64_content[index:index + crypter_offset]:
+            byte = str(hex(ord(char)))[2:]
+            if len(byte) < 2:
+                byte = '0' + byte
+            _str += '\\x' + str(byte)
+        code += f'{crypter_mark} += "{_str}"\n'
+        index += crypter_offset
     return code
 
 def pycrypt_startup():
@@ -43,21 +48,18 @@ def pycrypt_startup():
     print()
 	
     try:
-        path = argv[1]
-		
+        path = argv[1]	
         if not os.path.exists(path):
             print(f'[{Fore.RED}x{Fore.RESET}] Selected File {Fore.RED}NOT FOUND!{Fore.RESET}')
             exit()
         if not os.path.isfile(path) or not path.endswith('.py'):
             print(f'[{Fore.RED}x{Fore.RESET}] File Type {Fore.RED}INVALID!{Fore.RESET}')
             exit()
-			
-        with open(path, 'r', encoding='utf-8', errors='ignore') as file:
-            file_content = file.read()
-        pycrypt_content = pycrypt(file_content)
+        pycrypt_content = pycrypt(file.read())
         with open(f'{path.split(".")[0]}_pycrypted.py', 'w') as file:
             file.write(pycrypt_content)
         print(f'[{Fore.GREEN}+{Fore.RESET}] File Has been PyCrypted {Fore.GREEN}SUCCESSFULLY!{Fore.RESET}')
+	time.sleep(2)
     except:
         print(f'[{Fore.RED}x{Fore.RESET}] PyCrypter Correct Usage: {Fore.YELLOW}{argv[0]} <file>{Fore.RESET}')
 
